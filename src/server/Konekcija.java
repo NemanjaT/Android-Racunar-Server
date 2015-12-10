2985e1e8f5d9f35baa630b11b94f54c22b6ec8f2
masterkitty0102 <nemanjat94@hotmail.com>
2015-12-10 13:22:48 +0100

    Server v0.2
    Dodata mogucnost za veci broj konekcija. Port promenjen na 8888. Dodata
    klijentska klasa za testiranje (privremeno).

--- src/server/Konekcija.java
+++ src/server/Konekcija.java
@@ -1,22 +1,67 @@
 package server;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 
-public class Konekcija extends Socket {
+public class Konekcija implements Runnable {
 	private static int ID = 0;
 	
-	private int id;
+	private final int id;
+	private Socket konekcija;
+	private ObjectInputStream input;
+	//private ObjectOutputStream output;
+	private volatile boolean work;
 	
-	public Konekcija() {
-		super();
-		id = ID++;
+	public Konekcija(Socket konekcija) {
+		this.id = ID++;
+		this.work = false;
+		this.konekcija = konekcija;
 	}
 	
-	public int getId() {
-		return id;
+	public boolean works() {
+		return this.work;
 	}
 	
 	public String toString() {
-		return "Socket[" + id + "]";
+		return "Konekcija[" + id + "]";
+	}
+	
+	private void log(String poruka) {
+		System.err.print(this + ":");
+		System.out.println(" " + poruka);
+	}
+
+	@Override
+	public void run() {
+		work = true;
+		try {
+			try {
+				//output = new ObjectOutputStream(konekcija.getOutputStream());
+				//output.flush();
+				input = new ObjectInputStream(konekcija.getInputStream());
+				log("~Postavljen I/O Sistem!");
+				String poruka = "";
+				do {
+					try {
+						poruka = (String)input.readObject();
+						log(poruka);
+					} catch (ClassNotFoundException cnf) {
+						log("~Nepoznata klasa od strane klijenta!");
+					}
+				} while(!poruka.equals("END"));
+			} catch (EOFException eof) {
+				log("~Prekinuta konekcija sa klijentom");
+			} finally {
+				konekcija.close();
+				input.close();
+				//output.close();
+				log("~Uklonjen I/O Sistem.");
+				work = false;
+			}
+		} catch (IOException io) {
+			io.printStackTrace();
+		} 
 	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,60 +1,36 @@
 package server;
 
-import java.io.EOFException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	private final int port = 8080;
+	private final int port = 8888;
 	
 	private ServerSocket server;
-	private Socket[] konekcije;
-	private ObjectInputStream input;
-	//private ObjectOutputStream output;
+	private Konekcija[] konekcije;
 	
 	public Server() {
-		konekcije = new Socket[4];
-		for(int i = 0; i < konekcije.length; i++)
-			konekcije[i] = new Socket();
+		konekcije = new Konekcija[4];
+		this.startServer();
+	}
+	
+	public void startServer() {
 		try {
-			server = new ServerSocket(port, 4);
-			System.out.println("Ceka se konekcija na konekciji (port: " + port + ")...");
-			while( true ) {
-				konekcije[0] = server.accept();
-				System.out.println("Prihvacena konekcija: " + konekcije[0].getInetAddress());
-				/*output = new ObjectOutputStream(konekcije[0].getOutputStream());
-				output.flush();*/
-				input = new ObjectInputStream(konekcije[0].getInputStream());
-				System.out.println("I/O sistem postavljen!");
-				String poruka = "";
-				do {
-					try {
-						poruka = (String)input.readObject();
-						System.out.println(konekcije[0].getInetAddress() + ":\t" + poruka);
-					} catch (ClassNotFoundException e) {
-						System.err.println("Nije poznat tip podatka");
-					} catch (EOFException e2) {
-						System.err.println("Sistem vise ne prima poruke!");
+			server = new ServerSocket(port);
+			System.out.println("SERVER SLUSA NA PORTU: " + port);
+			while (true) {
+				Socket socket = server.accept();
+				for(int i = 0; i < konekcije.length; i++) {
+					if(konekcije[i] == null || !konekcije[i].works()) {
+						konekcije[i] = new Konekcija(socket);
+						new Thread(konekcije[i]).start();
+						break;
 					}
-				} while (!poruka.equals("END"));
-				System.out.println("*---. ZATVORENA KONEKCIJA .---*");
+				}
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				input.close();
-				//output.close();
-				server.close();
-				for(Socket s : konekcije)
-					if(!s.isClosed())
-						s.close();
-				System.out.println("*---. ZATVOREN I/O .---*");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		} catch (IOException io) {
+			io.printStackTrace();
 		}
 	}
 	
